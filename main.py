1954be45127b891f0cac4e1f8641afe98bec79ff
tsabina2197 <tsabina2197@gmail.com>
2025-12-10 17:46:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1 - Masala
+kvadratlar = [i ** 2 for i in range(1, 11)]
+print(kvadratlar)
+# 
+# 2 - Masala
+juft = [i for i in range(1, 11) if i % 2 == 0]
+print(juft)
+# 
+# 3 - Masala
+toq = [i for i in range(10, 31) if i % 2 != 0]
+print(toq)
+# 
+# 4 - Masala
+words = ["apple", "banana", "pear"]
+uzunlik = [len(s) for s in words]
+print(uzunlik)
+# 
+# 5 - Masala
+harflar = [harf for harf in "So'z"]
+print(harflar)
